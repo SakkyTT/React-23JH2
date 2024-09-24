9a369ed23c78423244372de0bb27359f6a98dc5e
SakkyTT <teemu.turunen@sakky.fi>
2024-09-24 19:47:37 +0300

    tabmenu onselect kesken

--- eka-react/src/components/App.jsx
+++ eka-react/src/components/App.jsx
@@ -4,16 +4,21 @@ import ClassComponent from './ClassComponent';
 import Counter from './Counter';
 import Product from './Product/Product';
 import HeaderTest from './HeaderTest/HeaderTest';
+import TabButton from './TabButton/TabButton';
 import { PRODUCTS } from '../data/data'; // dummy data
 
 function App() {
 
   // Täällä eläisi Products state
 
+  function handleSelect() {
+    console.log("Category clicked! - selected");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <HeaderTest/>
+        <HeaderTest />
         <Counter />
         {/* <EkaComponent/>
         <ClassComponent/>         */}
@@ -25,7 +30,7 @@ function App() {
               img={PRODUCTS[0].img}
               title={PRODUCTS[0].title}
               description={PRODUCTS[0].description} />
-              {/* Voi käyttää destructure ... syntaksia, jos
+            {/* Voi käyttää destructure ... syntaksia, jos
                   lähde datan avaimet ovat samat kuin componentin
                   avaimet. */}
             <Product
@@ -33,6 +38,18 @@ function App() {
             <Product
               {...PRODUCTS[2]} />
           </ul>
+        </section>
+        <section>
+          <h2>Kategoriat</h2>
+          <menu>
+            {/* 2. tapa: component composition */}
+            {/* kaikkea lapsi dataa ei tarvitse pyörittää propsien kautta */}
+            <TabButton onSelect={handleSelect}>Kategoria 1</TabButton>
+            <TabButton onSelect={handleSelect}>Kategoria 2</TabButton>
+            <TabButton onSelect={handleSelect}>Kategoria 3</TabButton>
+            <TabButton onSelect={handleSelect}>Kategoria 4</TabButton>
+          </menu>
+          {/* Tähän päivittyy uusi sisältö */}
         </section>
       </header>
     </div>
