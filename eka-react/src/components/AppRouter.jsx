0d7e467a662282f69353323fd99c41e1c5112320
SakkyTT <teemu.turunen@sakky.fi>
2024-10-08 17:33:37 +0300

    products route lisätty

--- eka-react/src/components/AppRouter.jsx
+++ eka-react/src/components/AppRouter.jsx
@@ -1,8 +1,20 @@
 // Root componentti
-function AppRouter(){
-    return <div>
 
-    </div>;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+// https://google.com <- domain
+// /search/jne <- route/path
+
+// Määritellään reittejä / polkuja (routes/paths)
+const router = createBrowserRouter([
+    { path: '/', element: <HomePage /> }, // polkuja taulukossa, polut annetaan map-muodossa
+    { path: '/products', element: <ProductsPage /> },
+]);
+
+function AppRouter(){
+    return <RouterProvider router={router} />;
 }
 
 export default AppRouter;
